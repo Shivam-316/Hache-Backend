e1cfa2d2a63a98cb26977939f9784a18c514bbcb
Shivammm <coolshiv77@gmail.com>
2020-12-09 18:25:53 +0530

    Initial Prod. Changes

--- ctft/settings.py
+++ ctft/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,14 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o46h5f--rp!!#va@w8o@&9_#4*t_bj5vs*5=nc=tow-c-zan7r'
-
+SECRET_KEY=os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=False
 
-ALLOWED_HOSTS = ['*']
-
-
+ALLOWED_HOSTS=['localhost','127.0.0.1','.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
